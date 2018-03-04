f56a5a7e46490b172e8dcb63ddda78a4e582a1b4
Nikolas Lada <nikolas.lada@gmail.com>
2018-03-04 19:13:36 +0100

    Extended functions for checking parameters.

--- src/ReflectionFactory.php
+++ src/ReflectionFactory.php
@@ -18,56 +18,85 @@ final class ReflectionFactory {
   /**
    * @param string $className
    * @param array|\ArrayObject $params
-   * @param \Closure $beforeCreateClosure
+   * @param \Closure $beforeCreate
+   * @param bool $isStrict
    * @return object
    * @throws \RuntimeException
    */
-  public function createWithChecking(string $className, $params, \Closure $beforeCreateClosure) {
+  public function createWithChecking(string $className, $params, \Closure $beforeCreate, $isStrict = FALSE) {
     $r = new \ReflectionClass($className);
     $method = $r->getConstructor();
     $required = $method->getParameters();
     
-    if (count($required) !== count($params)) {
-      throw new \RuntimeException;
-    }
-    
-    foreach ($required as $v) {
-      if (!isset($params[$v])) {
+    $interfaces = $r->getInterfaces();
+    if (array_key_exists('\Countable', $interfaces)) {
+      if (count($required) !== count($params)) {
         throw new \RuntimeException;
       }
       
     }
     
-    $beforeCreateClosure();
+    if (!array_key_exists('\ArrayAccess', $interfaces) || $isStrict) {
+      $input = $this->checkStrictly($required, $params);
+    } else {
+      $this->check($required, $params);
+      $input = $params;
+    }
     
-    return $r->newInstanceArgs($params);
+    $beforeCreate();
+    
+    return $r->newInstanceArgs($input);
   }
   
   /**
    * @param string $className
    * @param \stdClass $params
-   * @param \Closure $beforeCreateClosure
+   * @param \Closure $beforeCreate
    * @return object
    * @throws \RuntimeException
    */
-  public function createFromStdClassWithChecking(string $className, \stdClass $params, \Closure $beforeCreateClosure) {
+  public function createFromStdClassWithChecking(string $className, \stdClass $params, \Closure $beforeCreate) {
     $r = new \ReflectionClass($className);
     $method = $r->getConstructor();
     $required = $method->getParameters();
     
+    $input = $this->checkStrictly($required, $params);
+    $beforeCreate();
+    
+    return $r->newInstanceArgs($input);
+  }
+  
+  /**
+   * @param array $required
+   * @param \stdClass|\ArrayObject $params
+   * @throws \RuntimeException
+   * @return array
+   */
+  private function checkStrictly(array $required, $params): array {
     $input = [];
     foreach ($required as $v) {
-      if (isset($params->$v)) {
-        $input[$v] = $params->$v;
+      /* @var $v \ReflectionParameter */
+      $key = $v->getName();
+      if (isset($params->$key)) {
+        $input[] = $params->$key;
       } else {
         throw new \RuntimeException;
       }
       
     }
     
-    $beforeCreateClosure();
-    
-    return $r->newInstanceArgs($input);
+    return $input;
   }
   
+  private function check(array $required, $params) {
+    foreach ($required as $v) {
+      /* @var $v \ReflectionParameter */
+      $key = $v->getName();
+      if (!isset($params[$key])) {
+        throw new \RuntimeException;
+      }
+      
+    }
+  }
+
 }
